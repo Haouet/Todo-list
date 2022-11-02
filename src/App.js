a46597c4fa953f10055fa1f6a6410664ef576d46
sedrainfo <taherhaouet@gmail.com>
2022-11-02 14:33:54 +0100

    first commit todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useRef } from "react";
+
+import "./App.css";
+
+import TodoItem from "./components/TodoItem";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, removeTodo } from "./features/TodoSlice";
 
 function App() {
+  const desc = useRef(null);
+  //👇️ use state input
+  const [input, setInput] = useState(""); 
+  // 👇️use selector store
+  const count = useSelector((state) => state.todo.count);
+  const todos = useSelector((state) => state.todo.todos);
+  // 👇️dispatch store
+  const dispatch = useDispatch();
+ // 👇️ action sur input 
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+  // 👇️ clear all input values in the form
+    desc.current.value = '';
+    
+  };
+
+  const handleTodoDone = (id) => {
+    dispatch(removeTodo(id));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <h1>TODO List </h1>
+      <form className="App-form" onSubmit={handleAddTodo}>
+        <input  ref={desc} type="text" onInput={(e) => setInput(e.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+      
+      <div className="Todos">
+        {count > 0 &&
+          todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              text={todo.text}
+              id={todo.id}
+              onCheck={handleTodoDone}
+            />
+          ))}
+        {count === 0 && <p>No todos</p>}
+      </div>
     </div>
   );
 }
